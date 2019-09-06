a1c2903db6ca028cde1d3b1e40aafcbc1ca3d62f
Rashad <rashad.walcott15@gmail.com>
2019-09-06 15:39:27 -0400

    Progress

--- src/App.js
+++ src/App.js
@@ -2,13 +2,64 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
+
+const API = 'http://localhost:3000/pizzas'
 class App extends Component {
+
+  state = {
+    allPizzas:[],
+    editPizza: {},
+    vegetarian: null,
+    topping:"",
+    size: ''
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(pizzas => {
+      this.setState({
+        allPizzas: pizzas
+      })
+    })
+  }
+
+  editClickHandle = (pizza) => {
+    this.setState({
+      editPizza: pizza, vegetarian: pizza.vegetarian
+    })
+  }
+
+  changeVegetarian = () => {
+    this.setState({
+      vegetarian: !this.state.vegetarian
+    })
+  }
+
+  handleChange =() => {
+    console.log("changed");
+  }
+
+  handleSubmit =() => {
+    console.log("submitted");
+  }
+
   render() {
+    const vegetarian = this.state.vegetarian
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm pizza={this.state.editPizza}
+          vegetarian ={vegetarian}
+          changeVegetarian={this.changeVegetarian}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          />
+
+
+        <PizzaList pizzas={this.state.allPizzas}
+          editClickHandle = {this.editClickHandle}
+          />
       </Fragment>
     );
   }
